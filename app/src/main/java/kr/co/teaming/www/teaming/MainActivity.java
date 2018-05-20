de80e24263a1d4a01507b403227eff6f3aef3746
nadan <qskeksq@gmail.com>
2018-05-21 02:34:24 +0900

    test

--- app/src/main/java/kr/co/teaming/www/teaming/MainActivity.java
+++ app/src/main/java/kr/co/teaming/www/teaming/MainActivity.java
@@ -31,14 +31,6 @@ public class MainActivity extends AppCompatActivity
         NoticeFragment.OnFragmentInteractionListener,
         MessageFragment.OnFragmentInteractionListener {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
     /**
